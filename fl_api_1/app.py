148368a1b83a870327581dcc59bccac8aa12716a
dev <smirnovskoe@gmail.com>
2020-11-07 11:18:41 -0800

    created 2 services with Dockerfiles

--- fl_api_1/app.py
+++ fl_api_1/app.py
@@ -3,10 +3,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/api/health')
+@app.route('/')
 def r1_health():
     return jsonify({'Status': '1st is Up'})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9001)
+    app.run(debug=True, host='127.0.0.1', port=8001)
--- fl_api_2/app.py
+++ fl_api_2/app.py
@@ -3,10 +3,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/api/health')
+@app.route('/')
 def r2_health():
     return jsonify({'Status': '2nd is Up'})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9002)
+    app.run(debug=True, host='0.0.0.0', port=8002)
